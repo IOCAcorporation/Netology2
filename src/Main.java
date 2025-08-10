79d69939234213b50d43c74446a77769d2879f37
IOCAcorporation <hri.st@yandex.com>
2025-08-10 17:16:02 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 13000;
+        int miles;
+        miles = 20;
+        int answer;
+        answer = ticket / miles;
+        out.println("За билет начисляется -");
+        out.println(answer + " Миль");
+
+
+
+    }
+}
